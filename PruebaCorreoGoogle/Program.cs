0e6d08ede7db0f11c6c401edc37c1a3d4cf5ddc3
kike360 <30991393+kike360@users.noreply.github.com>
2017-10-13 22:46:50 -0500

    Agregar archivos de proyecto.

---
+++ PruebaCorreoGoogle/Program.cs
@@ -0,0 +1,45 @@
+﻿using Marzam.Utiles.Comunicaciones;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PruebaCorreoGoogle
+{
+    class Program
+    {
+
+        static string servidorcorreo = ConfigurationManager.AppSettings["servidorcorreo"].ToString();
+        static int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puertocorreo"].ToString());
+        static string correodestino = ConfigurationManager.AppSettings["correodestino"].ToString();
+        static string correode = ConfigurationManager.AppSettings["correoorigen"].ToString();
+        static string usuario = ConfigurationManager.AppSettings["usuario"].ToString();
+        static string password = ConfigurationManager.AppSettings["pass"].ToString();
+        static bool ssl = Convert.ToBoolean(ConfigurationManager.AppSettings["autentificacionssl"].ToString());
+
+
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Marzam.Utiles.Comunicaciones.Email email = new Email(correode, "", true, System.Net.Mail.DeliveryNotificationOptions.OnSuccess, System.Net.Mail.MailPriority.High, servidorcorreo, puerto, usuario, password, ssl);
+                email.Enviar(correodestino, "", "INVITO COCAS", "INVITO COCAS");
+                
+                
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+               
+            }
+           
+
+        }
+    }
+}
